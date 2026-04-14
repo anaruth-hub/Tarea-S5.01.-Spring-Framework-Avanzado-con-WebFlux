5aa58c064a9c42a7771704fd75b8dda32f2f07e6
Ana Ruth <aniarinna@gmail.com>
2026-04-14 22:35:01 +0200

    feat: add player andd game domain model

--- src/main/java/com/anaruth/blackjack/domain/model/player/Player.java
+++ src/main/java/com/anaruth/blackjack/domain/model/player/Player.java
@@ -1,4 +1,35 @@
 package com.anaruth.blackjack.domain.model.player;
 
 public class Player {
+
+    private final PlayerId id;
+    private PlayerName name;
+
+    public Player(PlayerId id, PlayerName name) {
+        if (id == null) {
+            throw new IllegalArgumentException("Player id cannot be null");
+        }
+        if (name == null) {
+            throw new IllegalArgumentException("Player name cannot be null");
+        }
+
+        this.id = id;
+        this.name = name;
+
+    }
+
+    public PlayerId getId() {
+        return id;
+    }
+
+    public PlayerName getName() {
+        return name;
+    }
+
+    public void rename(PlayerName newName) {
+        if (newName == null) {
+            throw new IllegalArgumentException("New player name cannot be null");
+        }
+        this.name = newName;
+    }
 }
--- src/main/java/com/anaruth/blackjack/domain/model/player/PlayerName.java
+++ src/main/java/com/anaruth/blackjack/domain/model/player/PlayerName.java
@@ -1,4 +1,39 @@
 package com.anaruth.blackjack.domain.model.player;
 
+import java.util.Objects;
+
 public class PlayerName {
+
+    private final  String value;
+
+    public PlayerName(String value) {
+        if(value == null || value.isBlank()) {
+            throw new IllegalArgumentException("Player name cannot be null or blanck");
+         }
+
+        String normalizedValue = value.trim();
+
+        if(normalizedValue.length() < 2 || normalizedValue.length() > 30) {
+            throw new IllegalArgumentException("Player namee must be between 2 and 30 characters");
+
+        }
+
+        this.value = normalizedValue;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof  PlayerName that)) return false;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
